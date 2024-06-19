b378813638df324b3b74810d2cb67eeac771483e
SS22 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-06-19 21:25:02 +0300

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -49,15 +49,6 @@ ${tradutor.texto1[8]} ${joincount}
 ${tradutor.texto1[9]} ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
  ${readMore}
 
- ${tradutor.texto1[10]}
-  
-  ▢ _${usedPrefix}menuaudios_
-  ▢ _${usedPrefix}menuanimes_
-  ▢ _${usedPrefix}labiblia_ (🔞)
-  ▢ _${usedPrefix}lang_ ${tradutor.texto2}
-  ▢ _${usedPrefix}langgroup_ ${tradutor.texto3}
-  ▢ _${usedPrefix}glx_ ${tradutor.texto4 /** Este menú permanecerá aquí por un corto tiempo, hasta que la gente sepa */}
-
  
   ${tradutor.texto1[11]}
 
